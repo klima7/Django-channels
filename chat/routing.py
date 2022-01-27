64965bc4643866170bd93b25e1f2e0abb659db44
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-01-27 02:00:32 +0100

    re_path changed to path

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
+from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi())
+    path(r'ws/chat/<room_name>/', consumers.ChatConsumer.as_asgi())
 ]
